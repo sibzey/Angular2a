5bce9e3bfcc4533ccb39a570746186c6cced414d
Sibel Öztürk <sibelmozturk@hotmail.com>
2024-03-12 00:55:03 +0300

    binding_bootstrap

--- angular2a/src/app/app.component.ts
+++ angular2a/src/app/app.component.ts
@@ -3,21 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //<app-root></app-root>
   standalone: true,
   imports: [RouterOutlet, FormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular2a';
-  count=0;
-  onBtnClick(){
-    this.count++;
-  }
-  onChange(event: Event){
-    console.log("input değeri değişti.");
+  
 
   }
-}
+
 
--- angular2a/src/app/components/form/form.component.ts
+++ angular2a/src/app/components/form/form.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-form',
+  selector: 'myform',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  username: string = '';
+  // Reactive Forms
 
+  writeToConsole() {
+    console.log(this.username);
+  }
+
+  changeFromTs() {
+    this.username = 'örnek';
+  }
 }
