7df6a9daec9e21832dafd68a6dfedabc06d0e1bc
Sibel Öztürk <sibelmozturk@hotmail.com>
2024-03-12 10:28:03 +0300

    AddedHttpClient

--- angular2a/src/app/app.component.ts
+++ angular2a/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root', //<app-root></app-root>
   standalone: true,
-  imports: [RouterOutlet, FormComponent],
+  imports: [RouterOutlet, FormComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- angular2a/src/app/pages/homepage/homepage.component.ts
+++ angular2a/src/app/pages/homepage/homepage.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from '../../models/toDo';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
 
+  todoList: ToDo[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getTodos();
+  }
+
+  getTodos() {
+    // backend'e istek atıp verileri çek
+    this.httpClient
+      .get<ToDo[]>('https://jsonplaceholder.typicode.com/todos')
+      .subscribe({
+        next: (response: ToDo[]) => {
+          console.log('Backendden cevap geldi:', response);
+          this.todoList = response;
+        },
+        error: (error) => {
+          console.log('Backendden hatalı cevap geldi:', error);
+        },
+        complete: () => {
+          console.log('Backend isteği sonlandı.');
+        },
+      });
+  }
+
+  postToDo() {
+    let obj = {};
+    this.httpClient.post('link', obj).subscribe();
+  }
 }
